1f39ff8a9cc8631151924f1a68f6644444a1b3cf
N3o D4rk3r <61631611+n3o-d4rk3r@users.noreply.github.com>
2021-01-08 19:52:39 +0600

    Update

--- contact-form/contact.php
+++ contact-form/contact.php
@@ -1,9 +1,10 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "sorkerbappy5001@gmail.com";
-$subject = "Contact Form";
+<?php 
+$contact_name = $_POST['contact_name'];
+$contact_email = $_POST['contact_email'];
+$contact_message = $_POST['contact_message'];
+$contact_formcontent="From: $contact_name \n Message: $contact_message";
+$contact_recipient = "sorkerbappy5001@gmail.com";
+$contact_subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
