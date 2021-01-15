1e23f57bb40b55080a2c625eb9909d04e84185a7
N3o D4rk3r <61631611+n3o-d4rk3r@users.noreply.github.com>
2021-01-15 23:50:11 +0600

    Update

--- contact-form/contact.php
+++ contact-form/contact.php
@@ -1,19 +1,19 @@
-<?php
-}
-else
-}
-$name=$_REQUEST['name'];
-$email=$_REQUEST['email'];
-$message=$_REQUEST['message'];
-if (($name=="")||($email=="")||($message==""))
-{
-echo "All fields are required, please fill <a href=\"\">the form</a> again.";  
-}
-else
-$from="From: $name\r\nReturn-path: $email";
-$subject="Message sent using your contact form";
-mail("sorkerbappy5001@gmail.com", $subject, $message, $from);
-echo "Email sent!";
-}
-}
+<?php 
+if(isset($_POST['submit'])){
+    $to = "ictd.kabir@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Get a Projects";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
